82e9fa9b3727c3eed7baedd0bff6e3aff2ccbbde
Weslley <weslley.washington01486@gmail.com>
2023-05-24 17:47:36 -0300

    COlocando os Post separado

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -17,6 +17,23 @@ export function Post () {
                     <time title='24 de maio as 17:20h' dateTime='2023-05-24 17:20:23'>Publicado a 1h</time>
                 </div>
             </header>
+            <div className={styles.content}>
+                <p>Fala Meu Povo!</p>
+
+                <p>Acabei de subir mais um projeto no meu portifólio. 
+                    É um projeto que fiz no Ignite da Rocketseat. 
+                    O nome do projeto é Ignite Feed 🚀                    
+                </p>
+                
+                <p> 
+                    <a href="">#novoprojeto</a> 
+                    <a href="">#nlw</a> 
+                    <a href=""> #rocketseat</a> 
+                </p>
+
+
+
+            </div>
         </article>
     )
 
